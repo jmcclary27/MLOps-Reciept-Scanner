2f30283a24f8c7fe18efbe9db29cd80f3d2ab815
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-10-04 17:25:29 +0530

    updated structure

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 __version__ = "0.0.0"
 
-REPO_NAME = "MLOps_Foundation"
+REPO_NAME = "MLOps-End-To-End-Project"
 AUTHOR_USER_NAME = "Asutosh"
 SRC_REPO = "mlproject"
 AUTHOR_EMAIL = "ashutoshsidhya69@gmail.com"
@@ -15,7 +17,9 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="Mlpos foundation project",
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/sidhyaashu/MLOps-End-To-End-Project.git",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
